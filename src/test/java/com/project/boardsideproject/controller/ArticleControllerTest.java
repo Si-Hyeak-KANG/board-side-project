8503e638624e16468cb5d2c6657d3d4deef30833
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-05-21 21:25:32 +0900

    #18 - update controller test
    미디어 타입 검사 방법을 40c0221 에서 확인했기 때문에,
    나머지 뷰도 동일하게 테스트 코드 업데이트

--- src/test/java/com/project/boardsideproject/controller/ArticleControllerTest.java
+++ src/test/java/com/project/boardsideproject/controller/ArticleControllerTest.java
@@ -39,7 +39,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -51,7 +51,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -61,7 +61,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
     }
 }
