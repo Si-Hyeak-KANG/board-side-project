c35e5cc4b55bcbbfe77a738e65fbc58f96c93783
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-05-23 22:55:18 +0900

    #20 - 서비스 테스트 작성 (Mockito 활용)
    - 낮은 결합, 빠른 테스트를 위한 테스트, Mockito 활용 습득
    - TDD 방법론 습득

---
+++ src/main/java/com/project/boardsideproject/service/ArticleService.java
@@ -0,0 +1,33 @@
+package com.project.boardsideproject.service;
+
+import com.project.boardsideproject.domain.type.SearchType;
+import com.project.boardsideproject.dto.ArticleDto;
+import com.project.boardsideproject.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+
+    }
+}
